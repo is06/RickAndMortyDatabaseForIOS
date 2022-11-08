ab72b0b51bfe8003485623b637e1bf0ee98e73e5
Thomas Noury <thomas.noury@protonmail.com>
2022-11-08 19:33:37 +0100

    Initial Commit

---
+++ RickAndMortyDatabase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyDatabase
+//
+//  Created by Thomas Noury  on 08/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
